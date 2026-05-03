02223bc9aa0fc3029d0e21fe3b01a85976375b5b
gloriayma <gloriama@mit.edu>
2026-05-03 17:35:56 -0400

    convenience helpers to load in gt structures

--- imputation/basic_helpers.py
+++ imputation/basic_helpers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from boltzgen.data.data import Structure
+from gloria_hbond_helpers import gloria_remove_weak_solvents
 
 NPZ_ROOT = Path("/data/rbg/shared/datasets/processed_rcsb/rcsb_solvents/structures")
 NPZ_PATH = None  # Set to an explicit file path to override PDB_ID + NPZ_ROOT.
@@ -12,9 +13,22 @@ def resolve_npz_path(pdb_id: str, npz_root: Path, npz_path: str | None = None) -
     return Path(npz_root) / f"{pdb_id.lower()}.npz"
 
 
-def stripped_gt_structure(PDB_ID):
+def raw_gt_structure(PDB_ID):
     npz_path = resolve_npz_path(PDB_ID, NPZ_ROOT, NPZ_PATH)
     gt_structure = Structure.load(npz_path)
+    print(f"Raw GT structure number of solvents: {gt_structure.count_solvents()}")
+    return gt_structure
+
+def stripped_gt_structure(PDB_ID):
+    gt_structure = raw_gt_structure(PDB_ID)
     gt_structure = gt_structure.to_one_solvent_per_chain(gt_structure)
     stripped_gt = gt_structure.remove_solvents()
+    assert stripped_gt.count_solvents() == 0
     return stripped_gt
+
+def filtered_gt_structure(PDB_ID, min_hbonds):
+    gt_structure = raw_gt_structure(PDB_ID)
+    gt_structure = gt_structure.to_one_solvent_per_chain(gt_structure)
+    filtered_gt = gloria_remove_weak_solvents(gt_structure, min_hbonds=min_hbonds)
+    print(f"Filtered (>={min_hbonds} H-bonds) GT structure number of solvents: {filtered_gt.count_solvents()}")
+    return filtered_gt
